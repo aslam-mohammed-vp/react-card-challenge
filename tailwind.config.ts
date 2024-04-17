582a8c42d6bc7b6464e41088dbe3cb02d66d9084
Mohammed Aslam V P <aslam-mohammed@192.168.1.2>
2024-04-17 09:20:22 +0530

    card initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,30 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+      colors: {
+        "pale-blue": "hsl(221, 100%, 96%)",
+        "royal-blue": "hsl(241, 81%, 54%)",
+        "slate-blue": "hsl(252, 100%, 67%)",
+        "light-red": {
+          default: "hsl(0, 100%, 67%)",
+          light: "hsl(0, 100%, 67%,.2)",
+        },
+        "orangey-yellow": {
+          default: "hsl(39, 100%, 56%)",
+          light: "hsl(39, 100%, 56%,.2)",
+        },
+        "green-teal": {
+          default: "hsl(166, 100%, 37%)",
+          light: "hsl(166, 100%, 37%,.2)",
+        },
+        "cobalt-blue": {
+          default: "hsl(234, 85%, 45%)",
+          light: "hsl(234, 85%, 45%,.2)",
+        },
+        white: "hsl(0, 0%, 100%)",
+        "violet-blue": "hsla(256, 72%, 46%, 1)",
+        "persian-blue": "hsla(241, 72%, 46%, 0)",
+        "dark-gray-blue": "hsl(224, 30%, 27%)",
       },
     },
   },
